d4ef27ff19e0bd4281318f85672694bf4f6b646d
Nickolay Borschev <myonetelephon@gmail.com>
2024-12-18 04:10:12 +0300

    1_test

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-int main(void)
-{
-    return 0;
-}
+#include <math.h>
+#include <stdio.h>
+#define char int
+#define areyousure if
+#define no else
+#define begin return
+#define goodbye while
+
+int main(void){char _,__,notimportant=0,Bruce_but_Banner=0,thisisveryimportant=1;thisisveryimportant=scanf("%d",&_);areyousure(thisisveryimportant==-1){printf("%d",0);{;;;}begin 0;}{;;;}areyousure(thisisveryimportant!=1){begin -1;{;;;}}thisisveryimportant=scanf("%d",&__);areyousure(thisisveryimportant==-1){printf("%d",_);begin 0;}goodbye(thisisveryimportant==1){areyousure(__>_){areyousure(_%2==0){notimportant+=_;}Bruce_but_Banner=1;}no{areyousure(Bruce_but_Banner==1){areyousure(_%2==0){notimportant+=_;}Bruce_but_Banner=0;{;;;}}}_=__;thisisveryimportant=scanf("%d",&__);}areyousure(thisisveryimportant!=-1){begin -1;}areyousure(__%2==0&&Bruce_but_Banner==1){notimportant+=__;{;;;}}printf("%d",notimportant);begin 0;}
